7275fdce4bd8b015d8766249af19188fcd925442
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-02-06 22:14:40 -0800

    Create layout for code functionality

---
+++ substrings.rb
@@ -0,0 +1,11 @@
+dictionary = ['h', 'hi', 'heller']
+
+def substrings(word, dictionary)
+  dictionary.each do |dictionary_word|
+    if dictionary_word.match?(word)
+    else
+    end
+  end
+
+  return matches
+end
