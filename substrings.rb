cddda4de0609bde81f2b1931795c7082b650b1bd
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-02-06 22:21:49 -0800

    Update/add count in matches hash for every matching substring

--- substrings.rb
+++ substrings.rb
@@ -4,6 +4,11 @@ matches = {}
 def substrings(word, dictionary)
   dictionary.each do |dictionary_word|
     if dictionary_word.match?(word)
+      if matches[dictionary_word]
+        matches[dictionary_word] += 1
+      else
+        matches[dictionary_word] = 1
+      end
     else
     end
   end
